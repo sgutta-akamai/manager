5fd871b8076c4eef9b8e21a0294ea76443fdb29d
Yash Kain <ykain@akamai.com>
2025-07-15 11:45:05 +0530

    Texts changes as per the updated UX

--- packages/manager/src/features/Waf/WafDetail/WafOverview/WafOverview.tsx
+++ packages/manager/src/features/Waf/WafDetail/WafOverview/WafOverview.tsx
@@ -8,7 +8,7 @@ export const WafOverview = () => {
     <Stack spacing={2}>
       <Paper>
         <Typography variant="subtitle1"> Summary </Typography>
-        <Typography variant="h2"> All resources protected </Typography>
+        <Typography variant="h2"> Protection active </Typography>
 
         <Box alignItems="center" display="flex" gap={1} marginTop={2}>
           <Typography variant="subtitle1"> Excluded: </Typography>
@@ -20,7 +20,8 @@ export const WafOverview = () => {
         <Box alignItems="center" display="flex" gap={1}>
           <Typography variant="subtitle1"> NodeBalancers: </Typography>
           <Typography color="textSecondary" variant="body1">
-            NBalancer-01 | NBalancer-02 | NBalancer-03
+            {/*// TODO: Add the relevant re-direction link for the NodeBalancers' page. */}
+            <Link to="">NBalancer-01</Link>
           </Typography>
         </Box>
       </Paper>
@@ -29,9 +30,11 @@ export const WafOverview = () => {
         <Typography variant="h2"> Protections </Typography>
         <Stack mt={1}>
           <Typography variant="body1">
-            When you apply web application firewall protections as part of your
-            security policy, the policy uses rules to examine specific requests
-            and determine what, if any, action to take. <br />
+            Specify how your web application firewall responds to traffic by
+            setting actions for attack groups. These are sets of firewall rules
+            that work together to identify and mitigate related attack types.
+            Review and update these settings regularly to stay protected against
+            evolving attack patterns. &nbsp;
             {/*// TODO: Add the relevant redirection link for 'Learn more', once available. */}
             <Link to="">Learn more</Link>
           </Typography>
